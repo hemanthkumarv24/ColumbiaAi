a97ba14c36f84c86c7e1607790f2e9dfb70d6204
Hemanth Kumar V <babubangaram688@gmail.com>
2026-02-19 15:54:13 +0530

    Added a file to test

---
+++ Backend/Controllers/CodeQLValidationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class CodeQLValidationController
+{
+    public void GetUser(string userInput)
+    {
+        // ❌ Intentional SQL Injection vulnerability for CodeQL validation
+        string query = "SELECT * FROM Users WHERE Name = '" + userInput + "'";
+
+        using (var connection = new SqlConnection("Server=.;Database=TestDb;Trusted_Connection=True;"))
+        {
+            connection.Open();
+            var command = new SqlCommand(query, connection);
+            command.ExecuteReader();
+        }
+    }
+}
+
+public class SecretsTest
+{
+    // ❌ Hardcoded secret (intentional)
+    private const string ApiKey = "SuperSecretProductionKey123!";
+}
+
+
+
+public class FileTest
+{
+    public string ReadFile(string fileName)
+    {
+        // ❌ Vulnerable: user controls file path
+        return File.ReadAllText("/app/data/" + fileName);
+    }
+}
